31a8f4c629bf26ef45fd1f01a0f5eee74af12857
Karan <kumarkay.ca@gmail.com>
2019-12-02 19:34:09 -0500

    displaying data

--- app/src/main/java/com/example/c0769647_f2019_mad3125_midterm/CRACustomer.java
+++ app/src/main/java/com/example/c0769647_f2019_mad3125_midterm/CRACustomer.java
@@ -425,7 +425,6 @@ public class CRACustomer implements Parcelable {
         {
             System.out.println("TotalTaxableIncome Less Than 10582");
         }
-
         
 
         return calculatedPtax;
