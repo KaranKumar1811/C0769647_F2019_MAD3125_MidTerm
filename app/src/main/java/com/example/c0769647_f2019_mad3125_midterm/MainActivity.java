c8b2e07a842dd81a9629dfbb7cd8cad034a1646b
Karan <kumarkay.ca@gmail.com>
2019-12-02 19:35:50 -0500

    displaying data

--- app/src/main/java/com/example/c0769647_f2019_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/example/c0769647_f2019_mad3125_midterm/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
-
 import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
